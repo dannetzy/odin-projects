e56949a75e30e7f5f2bb5307efd80efcf7de4df7
dannetzy <mdannefebrian@gmail.com>
2026-02-15 23:01:23 +0700

    finished etch logic, added random background mouseover

--- etch/script.js
+++ etch/script.js
@@ -1,15 +1,15 @@
 const container = document.querySelector(".sketch-container");
 const defaultSize = 16;
 const defaultWidthPx = 50;
+const defaultContainerWidthPx = defaultSize * defaultWidthPx;
 
 function makeGrid(size, width) {
   for (let i = 0; i < size; i++) {
     const column = document.createElement("div");
     column.classList.add("column");
-    for (let j = 1; j <= size; j++) {
+    for (let j = 0; j < size; j++) {
       const row = document.createElement("div");
       row.classList.add("row");
-      row.textContent = (i * size) + j;
       row.style.height = `${width}px`;
       row.style.width = `${width}px`;
       column.append(row);
@@ -18,11 +18,35 @@ function makeGrid(size, width) {
   }
 }
 
+function randomRGB() {
+  return Math.floor(Math.random() * 255);
+}
+
 container.addEventListener("mouseover", (event) => {
   if (event.target.classList.contains("row")) {
-    event.target.style.backgroundColor = "green";
+    event.target.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
   }
 });
 
 makeGrid(defaultSize, defaultWidthPx);
 
+const sketchBtn = document.querySelector("#sketchBtn");
+
+sketchBtn.addEventListener("click", () => {
+  const askSize = Number(prompt("Sketch size", 16));
+
+  if (isNaN(askSize)) {
+    alert("Please type a number from 1-100.");
+    return;
+  }
+  if (askSize > 100) {
+    alert("The maximum size is 100.");
+    return;
+  }
+  if (askSize < 1) {
+    alert("The minimum size is 1.");
+    return;
+  }
+  container.replaceChildren();
+  makeGrid(askSize, defaultContainerWidthPx / askSize);
+});
